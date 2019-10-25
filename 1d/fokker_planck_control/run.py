0ed73a1cfccad6c099295dee0c47e3936eca77ea
Praveen C <cpraveen@gmail.com>
2019-10-25 16:02:27 +0530

    Fix to work with python3

--- 1d/fokker_planck_control/run.py
+++ 1d/fokker_planck_control/run.py
@@ -40,32 +40,32 @@ initc = 0                     # Set initial condition internally
 # Run without optimization
 if len(sys.argv) > 1:
     if sys.argv[1]=="nocontrol":
-        print "Running uncontrolled case ..."
+        print("Running uncontrolled case ...")
         tstart, tend = 0.0, T
         c     = cost_value(u)
         os.system("cp sol.dat sol_nocontrol.dat")
-        print "Cost = ", c
+        print("Cost = ", c)
         exit()
 
-print "Running optimization ..."
+print("Running optimization ...")
 t = np.linspace(0.0, T, nint+1, True)
 
 u_all = np.zeros((nint,2))
 c_all = np.zeros(nint)
 for i in range(nint):
     tstart, tend = t[i], t[i+1]
-    print 'tstart, tend =', tstart, tend
+    print('tstart, tend =', tstart, tend)
     res = sop.minimize(cost_value, u, \
                        method='BFGS', jac=cost_gradient, \
                        options={'disp': True})
     u = res.x; c = cost_value(u)
-    print "   Control variable = ", u
-    print "   Objective func   = ", c
+    print("   Control variable = ", u)
+    print("   Objective func   = ", c)
     u_all[i,:] = u; c_all[i] = c;
     # Final solution becomes initial condition for next interval
     os.system("cp sol.dat init.dat")
     initc = 1 # From now on, read initial cond from file
-    print "----------------------------------------------"
+    print("----------------------------------------------")
 
 # Save final solution
 os.system("cp sol.dat sol_control.dat")
@@ -73,7 +73,7 @@ os.system("cp sol.dat sol_control.dat")
 # Save history to file
 f = open('opt.dat','w')
 for i in range(nint):
-    print u_all[i,:], c_all[i]
+    print(u_all[i,:], c_all[i])
     st = str(u_all[i,0])+" "+str(u_all[i,1])+" "+str(c_all[i])+"\n"
     f.write(str(t[i])  +" "+st)
     f.write(str(t[i+1])+" "+st)
