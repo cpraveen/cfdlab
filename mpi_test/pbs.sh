ac4ed0e66af422917e20d3cf9b64685663b1f3db
Praveen C <cpraveen@gmail.com>
2020-02-12 09:49:13 +0530

    Update pbs script

--- mpi_test/pbs.sh
+++ mpi_test/pbs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #PBS -V
+#PBS -m ae
+#PBS -M cpraveen@gmail.com
 #PBS -N mpi_test
 #PBS -o pbs_out.txt
 #PBS -e pbs_error.txt
@@ -7,7 +9,25 @@
 #PBS -q test
 cd $PBS_O_WORKDIR
 echo "Job began"
-date
+echo "Working directory is $PBS_O_WORKDIR"
+
+# Calculate the number of processors allocated to this run.
+NPROCS=`wc -l < $PBS_NODEFILE`
+
+# Calculate the number of nodes allocated.
+NNODES=`uniq $PBS_NODEFILE | wc -l`
+
+### Display the job context
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo Using ${NPROCS} processors across ${NNODES} nodes
+
+# OpenMPI will automatically launch processes on all allocated nodes.
+# MPIRUN=`which mpirun`
+# ${MPIRUN} -machinefile $PBS_NODEFILE -np ${NPROCS} my-openmpi-program
+
 mpiexec ./hello
+
 echo "Job ended"
-date
+echo Time is `date`
