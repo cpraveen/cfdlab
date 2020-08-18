79b0784dbbf45611330cf4d13b0e4320702c57c7
Praveen C <cpraveen@gmail.com>
2020-08-18 10:04:12 +0530

    Add comments

--- bin/texgraphics.sh
+++ bin/texgraphics.sh
@@ -5,7 +5,16 @@
 #    texgraphics *.tex
 #    texgraphics.sh paper1.tex paper2.tex ...
 #
-# Ignore comments, but will not work if comment % is at end of line
+# Do not use tab/indenting in tex files.
+# Do not put graphics filename extension, use it like this
+#
+#    \includegraphics[width=0.5\textwidth]{foo}
+#
+# Ignores commented includegraphics line, but will not work if comment % is at 
+# end of line, e.g., this line will be ignored also
+#
+#    \includegraphics[width=0.5\textwidth]{foo}  % this is a comment
+#
 fnames=`grep includegraphics $@ | grep -v % | sed 's/^[^{]*{\([^{}]*\)}.*/\1/'`
 files=
 for f in $fnames
