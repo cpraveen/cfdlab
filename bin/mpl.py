1a101f856e7661b8cf89343cfe7b41cbc83a41f0
Praveen C <cpraveen@gmail.com>
2021-05-31 08:05:05 +0530

    Add mpl settings

---
+++ bin/mpl.py
@@ -0,0 +1,9 @@
+from matplotlib import rcParams
+rcParams['font.size'] = 14
+rcParams['font.family'] = 'serif'
+rcParams['figure.autolayout'] = True
+rcParams['lines.linewidth'] = 2
+rcParams['lines.markersize'] = 6
+rcParams['axes.titlesize'] = 14
+rcParams['axes.labelsize'] = 14
+#rcParams['text.usetex'] = True    # This will use Latex fonts (slow)
