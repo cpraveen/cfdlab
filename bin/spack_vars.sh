9dd6fda212327449617812e50096b96412d2f067
Praveen C <cpraveen@gmail.com>
2021-06-22 15:50:06 +0530

    Add some more dirs

--- bin/spack_vars.sh
+++ bin/spack_vars.sh
@@ -2,6 +2,9 @@ echo export PETSC_DIR=$(spack location -i petsc)
 echo export SLEPC_DIR=$(spack location -i slepc)
 echo export HDF5_DIR=$(spack location -i hdf5)
 echo export METIS_DIR=$(spack location -i metis)
+echo export EIGEN_DIR=$(spack location -i eigen)
+echo export BLAS_DIR=$(spack location -i openblas)
+echo export LAPACK_DIR=$(spack location -i openblas)
 
 PACKAGES="cmake openmpi "
 
