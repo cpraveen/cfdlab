caf0f6c76588a364ad454af6b38ccf6d31a0391e
Praveen C <cpraveen@gmail.com>
2021-08-25 14:05:01 +0530

    Enable latex fonts

--- bin/mpl.py
+++ bin/mpl.py
@@ -6,4 +6,4 @@ rcParams['lines.linewidth'] = 2
 rcParams['lines.markersize'] = 6
 rcParams['axes.titlesize'] = 14
 rcParams['axes.labelsize'] = 14
-#rcParams['text.usetex'] = True    # This will use Latex fonts (slow)
+rcParams['text.usetex'] = True    # This will use Latex fonts (slow)
