cc48b0bbc89464416a898429ffe158fa252bc130
Praveen C <cpraveen@gmail.com>
2022-03-21 14:41:42 +0530

    Use 25 for all modes

--- bin/pmset.sh
+++ bin/pmset.sh
@@ -1,5 +1,4 @@
-sudo pmset -c hibernatemode 3
-sudo pmset -b hibernatemode 25
+sudo pmset -a hibernatemode 25
 sudo pmset -a standby       1
 sudo pmset -b tcpkeepalive  0
 sudo pmset -c gpuswitch     2
