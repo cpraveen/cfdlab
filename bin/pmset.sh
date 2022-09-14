c1ddc9222ad27ba89aa077a70bcf5db2fb432cab
Praveen C <cpraveen@gmail.com>
2022-09-14 11:59:08 +0530

    Disable powernap

--- bin/pmset.sh
+++ bin/pmset.sh
@@ -1,5 +1,6 @@
 sudo pmset -a hibernatemode 25
 sudo pmset -a standby       1
-sudo pmset -b tcpkeepalive  0
-sudo pmset -c gpuswitch     2
-sudo pmset -b gpuswitch     0
+sudo pmset -a tcpkeepalive  0
+sudo pmset -a powernap      0
+#sudo pmset -c gpuswitch     2
+#sudo pmset -b gpuswitch     0
