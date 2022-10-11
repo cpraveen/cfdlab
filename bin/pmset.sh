2c2565d488d13ab7ecb69e976b000f19d0eb2cf8
Praveen C <cpraveen@gmail.com>
2022-10-11 10:10:12 +0530

    Update pmset

--- bin/pmset.sh
+++ bin/pmset.sh
@@ -1,6 +1,19 @@
-sudo pmset -a hibernatemode 25
+# Default = 3, 
+# 25 is slow to wake
+sudo pmset -c hibernatemode 3
+sudo pmset -b hibernatemode 25
+
+# Default = 1
 sudo pmset -a standby       1
+sudo pmset -a standbydelay  1800
+
+# Dont wake for network access
 sudo pmset -a tcpkeepalive  0
+
 sudo pmset -a powernap      0
+
+sudo pmset -b lowpowermode  1
+
+# For intel machines which have discrete gpu
 #sudo pmset -c gpuswitch     2
 #sudo pmset -b gpuswitch     0
