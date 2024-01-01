3b6c9eab2affaa04a2194d977d4203f886402acb
Praveen C <cpraveen@gmail.com>
2024-01-01 18:37:05 +0530

    firedrake install script

---
+++ bin/firedrake.sh
@@ -0,0 +1,20 @@
+unset PETSC_DIR
+unset HDF5_DIR
+
+PATH=$PATH:`spack location -i autoconf`/bin
+PATH=$PATH:`spack location -i automake`/bin
+PATH=$PATH:`spack location -i gmake`/bin
+PATH=$PATH:`spack location -i libtool`/bin
+echo $PATH
+
+#PETSC_CONFIGURE_OPTIONS="--with-hdf5-dir=$HDF5_DIR"
+
+python ./firedrake-install --venv-name firedrake \
+                           --slepc \
+                           --with-parmetis \
+                           --mpicc mpicc \
+                           --mpicxx mpicxx \
+                           --mpif90 mpif90 \
+                           --mpiexec mpiexec \
+                           --with-blas /opt/spack \
+                           --no-package-manager
