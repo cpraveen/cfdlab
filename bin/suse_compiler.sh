c41d5531ecae78216bd9a0847de66dc92ac436c7
Praveen C <cpraveen@gmail.com>
2024-01-14 11:15:10 +0530

    Update script

--- bin/suse_compiler.sh
+++ bin/suse_compiler.sh
@@ -4,3 +4,6 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$VER $VER
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-$VER $VER
 sudo update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-$VER $VER
 
+sudo update-alternatives --config gcc && \
+sudo update-alternatives --config g++ && \
+sudo update-alternatives --config gfortran
