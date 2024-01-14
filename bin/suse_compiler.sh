2d324f610a16fb00f8f069c9d1b7324afdac037f
Praveen C <cpraveen@gmail.com>
2024-01-14 10:54:40 +0530

    Set compiler on suse

---
+++ bin/suse_compiler.sh
@@ -0,0 +1,6 @@
+VER=$1
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$VER $VER
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-$VER $VER
+sudo update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-$VER $VER
+
