e9b59d66b54d06417d3bdd05c3f2747174be8ec0
Praveen C <cpraveen@gmail.com>
2024-01-15 10:46:41 +0530

    delete ps eps at end

--- bin/pdf2ps2eps2pdf.sh
+++ bin/pdf2ps2eps2pdf.sh
@@ -2,8 +2,8 @@
 #   pdf --> ps --> eps --> pdf 
 # and then runs pdfbb, which must be in your path.
 # This ensures all fonts/symbols are embedded in the pdf.
-# Note that if you have a ps or eps file of same name, it will be deleted and
-# overwritten.
+#
+# NOTE THAT IF YOU HAVE A PS OR EPS FILE OF SAME NAME, IT WILL BE DELETED.
 
 # Find all pdf in this and all subdirectories
 
@@ -19,4 +19,5 @@ do
    ps2eps -O $BASENAME.ps 
    ps2pdf $BASENAME.eps $BASENAME.pdf
    pdfbb $BASENAME.pdf
+   rm -f $BASENAME.ps $BASENAME.eps
 done
