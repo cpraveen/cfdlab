627b4ae9147bff43a69ade6b762cab2394e465a0
Praveen C <cpraveen@gmail.com>
2024-01-15 10:25:47 +0530

    Add pdf scripts

--- bin/pdf2pptx.sh
+++ bin/pdf2pptx.sh
@@ -4,9 +4,11 @@
 #   soffice (from libreoffice)
 
 FILENAME=$1  # Include .pdf extension
-RAND=$RANDOM # Needed to avoid overwriting errors
+RAND=$RANDOM # Needed to avoid overwriting 
 NEW_FILE=/tmp/${RAND}_${FILENAME}
 gs -o $NEW_FILE -dNoOutputFonts -sDEVICE=pdfwrite $FILENAME
+#gs -o $NEW_FILE -dBATCH -dNoOutputFonts -dPDFSETTINGS=/prepress -dEmbedAllFonts=true -sDEVICE=pdfwrite $FILENAME
+#gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$NEW_FILE -c ".setpdfwrite </NeverEmbed []>> setdistillerparams" -f $FILENAME
 soffice --infilter=impress_pdf_import --convert-to pptx --outdir /tmp $NEW_FILE
 PPTX_NAME_WITH_RAND="${NEW_FILE%.*}"
 PPTX_NAME_WITHOUT_RAND="${FILENAME%.*}"
