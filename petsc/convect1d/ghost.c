299ab034c63e301ac7af460cd1c6c4efb07a1c72
Praveen C <cpraveen@gmail.com>
2024-02-02 12:17:52 +0530

    Add comments

--- petsc/convect1d/ghost.c
+++ petsc/convect1d/ghost.c
@@ -8,15 +8,18 @@ int main()
    int n  = ng + nx + ng;
    double *u = (double*)malloc(n * sizeof(double*)) + ng;
 
+   // Real cells
    for(int i=0; i<nx; ++i)
       u[i] = i;
 
-   // fill ghost values
+   // fill ghost values at left boundary
    for(int i=-ng; i<0; ++i)
       u[i] = u[i+nx];
+   // fill ghost values at right boundary
    for(int i=nx; i<nx+ng; ++i)
       u[i] = u[i-nx];
 
+   // Print all cells
    for(int i=-ng; i<nx+ng; ++i)
       printf("%d  %e\n", i, u[i]);
    return 0;
