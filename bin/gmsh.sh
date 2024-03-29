fd3731f94b04746f0fa8b3fa64c8fd16c3a50c69
Praveen C <cpraveen@gmail.com>
2024-03-29 13:28:19 +0530

    Minor updates

--- bin/gmsh.sh
+++ bin/gmsh.sh
@@ -1,2 +1,2 @@
-wget http://gmsh.info/bin/MacOSX/gmsh-stable-MacOSX.dmg -P $HOME/Downloads
-open $HOME/Downloads/gmsh-stable-MacOSX.dmg
+wget http://gmsh.info/bin/MacOSX/gmsh-stable-MacOSARM.dmg -P $HOME/Downloads
+open $HOME/Downloads/gmsh-stable-MacOSARM.dmg
