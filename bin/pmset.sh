d6f4e48a6b8a4af9f83f354d5c2a0665a2aacc2d
Praveen C <cpraveen@gmail.com>
2024-12-13 13:26:35 +0530

    Update pmset

--- bin/pmset.sh
+++ bin/pmset.sh
@@ -1,8 +1,8 @@
-# Default = 3, 
-# 25 is slow to wake
-sudo pmset -a hibernatemode 3
-#sudo pmset -c hibernatemode 3
-#sudo pmset -b hibernatemode 25
+# hibernatemode
+#   3, safe sleep, copy to disk, RAM is on
+#   25, copy to disk, RAM is off, slow to wake, good for battery
+sudo pmset -c hibernatemode 3
+sudo pmset -b hibernatemode 25
 
 # Default = 1
 sudo pmset -a standby       1
@@ -14,6 +14,9 @@ sudo pmset -a tcpkeepalive  0
 sudo pmset -a powernap      0
 
 sudo pmset -b lowpowermode  1
+
+sudo pmset -a sleep         1
+sudo pmset -a disablesleep  0
 
 # For intel machines which have discrete gpu
 #sudo pmset -c gpuswitch     2
