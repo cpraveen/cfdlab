c3f1639c724e906f531ae96ed19cfed18bc740f6
Praveen C <cpraveen@gmail.com>
2025-03-13 09:36:43 +0530

    Print hostname

--- mpi_test/hello.c
+++ mpi_test/hello.c
@@ -1,15 +1,17 @@
-/*The Parallel Hello World Program*/
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char **argv)
-{
-   int node;
-   
-   MPI_Init(&argc,&argv);
-   MPI_Comm_rank(MPI_COMM_WORLD, &node);
-     
-   printf("Hello World from Node %d\n",node);
-            
-   MPI_Finalize();
+int main(int argc, char** argv) {
+   int rank, size, ierr, plen;
+   char pname[MPI_MAX_PROCESSOR_NAME];
+
+   ierr = MPI_Init(&argc, &argv);
+   ierr = MPI_Comm_size(MPI_COMM_WORLD, &size);
+   ierr = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   ierr = MPI_Get_processor_name(pname, &plen);
+
+   printf("Hello World, from rank %d of %d procs, host %s\n", rank, size,
+          pname);
+   ierr = MPI_Finalize();
+   return 0;
 }
