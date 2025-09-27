0d0ee7d6dbc07de99b927b227c4a460bf12dd548
Praveen C <cpraveen@gmail.com>
2025-09-27 16:50:24 +0530

    Update comments

--- bin/firedrake_debian.sh
+++ bin/firedrake_debian.sh
@@ -17,6 +17,25 @@
 #
 #    . /root/firedrake/bin/activate
 #
+# Add to your .bashrc file
+# export OMP_NUM_THREADS=1
+#
+# CREATE AN IMAGE
+#
+# You can create an image out of this container
+#
+#    docker commit debian firedrake
+#
+# Run the image (you may want to share some host folder)
+#
+#    docker run -it --name firedrake -p 8888:8888 firedrake:latest
+#
+# You can start jupyter inside container 
+#
+#    . /root/firedrake/bin/activate
+#    jupyter-lab --ip 0.0.0.0 --port 8888 --no-browser --allow-root
+#
+# and access it from the host at http://localhost:8888/tree
 cd
 apt update
 apt install curl git python3 python3-venv vim
@@ -39,5 +58,5 @@ pip cache remove libsupermesh
 pip cache remove firedrake
 export $(python3 firedrake-configure --show-env)
 pip install --no-binary h5py 'firedrake[check]'
-pip install matplotlib
+pip install numpy scipy sympy matplotlib jupyterlab vtk
 firedrake-check
